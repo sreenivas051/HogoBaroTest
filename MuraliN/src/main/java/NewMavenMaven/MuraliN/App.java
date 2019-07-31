608fc6654ed504d991a04929147990cbd21b9833
sreenivas <sreenivas@DESKTOP-BMLLM5O>
2019-07-31 19:36:05 +0530

    Murali Test

---
+++ MuraliN/src/main/java/NewMavenMaven/MuraliN/App.java
@@ -0,0 +1,13 @@
+package NewMavenMaven.MuraliN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MuraliN/src/test/java/NewMavenMaven/MuraliN/NuraliTestNugTest.java
@@ -0,0 +1,17 @@
+package NewMavenMaven.MuraliN;
+
+import org.testng.annotations.Test;
+
+public class NuraliTestNugTest {
+
+	@Test()
+	public void naughtyTest() {
+		System.out.println("first one");
+		
+	}
+	@Test()
+	public void naughty123Test() {
+		System.out.println("first one 12345");
+		
+	}
+}
